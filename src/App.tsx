59af59e5f2cedafe942a20caa6b4143d04f40274
Renata Mie <renata_mie@hotmail.com>
2021-05-21 22:25:46 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import {uuid} from "uuidv4";
+import {api} from "./services/api";
 
-function App() {
+interface IData {
+  id: string;
+  name: string;
+  price: number;
+}
+
+const App: React.FC = () => {
+  const [data, setData] = useState<IData[]>([]);
+  const [fruta, setFruta] = useState<string>("");
+  const [frutaValue, setFrutaValue] = useState<any>();
+
+  useEffect(() => {
+    console.log(data)
+    api.get("data").then(
+      response => {
+        setData(response.data)
+      }
+    )
+  }, [data]);
+
+  const convertToCurrency= useCallback(
+    (value: number) => Intl.NumberFormat("pt-br", {style: "currency", currency: "BRL"}).format(value),
+    [],
+  )
+
+  const addToApi= useCallback(
+    () => {
+      api.post("data", {
+        id: uuid,
+        name: fruta,
+        price: frutaValue
+      }).then (
+        response => alert("tudo certo")
+      ).catch (e => alert ("error"))
+  }, [uuid, fruta, frutaValue]
+  )
+
+//  function convertToCurrency(value: number){
+//   return Intl.NumberFormat("pt-br", {style: "currency", currency: "BRL"}).format(value)
+//  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Join us too:</h1>
+
+      <ul >
+      {data.map(fruta => (
+        <li key={fruta.id}>
+          {fruta.name} | {convertToCurrency(fruta.price)}
+        </li> 
+      ))}
+      </ul>
+
+      <hr/>
+        <h1>{fruta}</h1>
+      <hr/>
+
+      <input type="text" onChange={e => setFruta(e.target.value)} placeholder="informe uma fruta" />
+      
+      <input type="number" onChange={e => setFrutaValue(parseFloat(e.target.value))} placeholder="informe um valor" />
+    
+      <button onClick={addToApi}>Add</button>
     </div>
   );
 }
 
 export default App;
+
+
